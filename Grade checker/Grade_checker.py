b34b87f56276ca7c2929fcae423febb74e6bd65f
Casey Hutton <50571062@student.southessex.ac.uk>
2023-11-28 16:37:55 +0000

    Changed with checsk

--- Grade checker/Grade_checker.py
+++ Grade checker/Grade_checker.py
@@ -1,3 +1,8 @@
+eng = {}
+mth = {}
+sci = {}
+totalFail=0
+
 def Carramel_Dansen_english(A,B,C):
     failed = 0
     if A >= 104:
@@ -19,7 +24,7 @@ def Carramel_Dansen_english(A,B,C):
     Avg = Total // 3
     print(f"Your total was {Total}/360")
     print(f"Your average was {Avg}/120")
-    return failed
+    return failed, Avg, Total
 
 def Carramel_Dansen_math(A,B,C):
         failed = 0
@@ -42,7 +47,7 @@ def Carramel_Dansen_math(A,B,C):
         Avg = Total // 3
         print(f"Your total was {Total}/300")
         print(f"Your average was {Avg}/100")
-        return failed
+        return failed, Avg, Total
 
 def Carramel_Dansen_science(A,B,C):
             failed = 0
@@ -61,31 +66,76 @@ def Carramel_Dansen_science(A,B,C):
             else:
                 print("Failed science test 3 :(")
                 failed+=1
-                Total = A+B+C
+            Total = A+B+C
             Avg = Total // 3
             print(f"Your total was {Total}/450")
             print(f"Your average was {Avg}/150")
-            return failed
+            return failed, Avg, Total
 
-failed = 0
+def inputGrade(subject):
+    eng = {}
+    mth = {}
+    sci = {}
+    if subject == "english":
+        for x in range(3):
+            while True:
+                try:
+                    eng[x] = int(input(f"Enter your first {x+1} english paper mark: "))
+                    if eng[x]<0 or eng[x]>120:
+                        print("There is no such thing as a negative mark")
+                    else:
+                        break
+                except ValueError:
+                    print("Invalid Number")
+        return eng
+    if subject == "math":
+        for x in range(3):
+            while True:
+                try:
+                    mth[x] = int(input(f"Enter your first {x+1} math paper mark: "))
+                    if mth[x]<0 or mth[x]>100:
+                        print("There is no such thing as a negative mark")
+                    else:
+                        break
+                except ValueError:
+                    print("Invalid Number")
+        return mth
+    if subject == "science":
+        for x in range(3):
+            while True:
+                try:
+                    sci[x] = int(input(f"Enter your {x+1} science paper mark: "))
+                    if sci[x]<0 or sci[x]>150:
+                        print("There is no such thing as a negative mark")
+                    else:
+                        break
+                except ValueError:
+                    print("Invalid Number")
+        return sci
+
+failed = 0  
+MthAverage = 0
+MthTotal= 0
+print("\nTotal per math exam is 100 marks")
+mth = inputGrade("math")
+failed, MthAverage, MthTotal = Carramel_Dansen_math(mth[0], mth[1], mth[2])
+totalFail+=failed
+
+EngAvg = 0
+EngTotal=0
 print("\nTotal per english exam is 120 marks")
-eng1 = int(input("Enter your first english paper mark: "))
-eng2 = int(input("Enter your second english paper mark: "))
-eng3 = int(input("Enter your third english paper mark: "))
-failed += Carramel_Dansen_english(eng1,eng2,eng3)
+eng = inputGrade("english")
+failed, EngAvg, EngTotal = Carramel_Dansen_english(eng[0],eng[1],eng[2],)
+totalFail+=failed
 
-print("\nTotal per maths exam is 100 marks")
-mth1 = int(input("Enter your first math paper mark: "))
-mth2 = int(input("Enter your second math paper mark: "))
-mth3 = int(input("Enter your third math paper mark: "))
-failed += Carramel_Dansen_math(mth1,mth2,mth3)
-
+SciAverage=0
+SciTotal=0
 print("\nTotal per science exam is 150 marks")
-sci1 = int(input("Enter your first science paper mark: "))
-sci2 = int(input("Enter your second science paper mark: "))
-sci3 = int(input("Enter your third science paper mark: "))
-failed += Carramel_Dansen_science(sci1,sci2,sci3)
+sci = inputGrade("science")
+failed, SciAverage, SciTotal = Carramel_Dansen_science(sci[0],sci[1],sci[2])
+totalFail+=failed
 
-print(f"\nYou failed {failed} tests out of 9")
-if failed>=3:
+print(f"\nYou failed {totalFail} tests out of 9")
+if totalFail>=3:
     print("You need to resit :3")
+print(">:3")
